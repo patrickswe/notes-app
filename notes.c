4358cbc92c84c623f2a379411178f694bfcb0a71
Patrick Sweeney <patrick.sweeney1983@gmail.com>
2025-08-11 19:23:15 +0100

    updated notes.c with requirements in comments

---
+++ notes.c
@@ -0,0 +1,21 @@
+// includes
+
+
+//propt the user to enter a note (100 chars)
+
+
+// save the note into a texxt file
+
+
+
+//Each new note shouyld be added on a new line
+
+
+
+//View Notes
+//Read all notes from notes.txt
+//Display in order with a note number before each one
+
+
+//menu - repeat until user chooses exit
+
